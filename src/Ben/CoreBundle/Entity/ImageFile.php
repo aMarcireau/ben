33c0e530835256616c84fa8b9192e67112bd4a77
Zboubidoo <alexandre.marcireau@student.ecp.fr>
2014-02-18 23:24:06 +0100

    Add file upload methods

--- src/Ben/CoreBundle/Entity/ImageFile.php
+++ src/Ben/CoreBundle/Entity/ImageFile.php
@@ -4,6 +4,9 @@ namespace Ben\CoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Ben\CoreBundle\Entity\Project;
 
 /**
  * ImageFile
@@ -12,6 +15,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  *
  * @ORM\Table()
  * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
  * @UniqueEntity("filename")
  */
 class ImageFile
@@ -51,6 +55,128 @@ class ImageFile
      * @ORM\ManyToOne(targetEntity = "Project", inversedBy = "imageFiles")
      */
     private $project;
+
+    
+    /**
+     * @Assert\Image(
+     *     maxWidth = 5000,
+     *     maxHeight = 5000,
+     *     maxSize = "1024k",
+     *     mimeTypesMessage = "Le fichier doit être une image"
+     *     maxWidthMessage = "L'image ne doit pas excéder {{ max_width }}px de large",
+     *     maxHeightMessage = "L'image ne doit pas excéder {{ max_height }}px de haut",
+     *     sizeNotDetectedMessage = "La taille de l'image n'a pas pu être detectée",
+     * )
+     * * @Assert\NotBlank(
+     *     message = "Une image doit être uploadée",
+     * )
+     */
+    public $file;
+    
+    
+    //
+    // Custom methods
+    //
+    
+    /**
+     * Get image directory name
+     *
+     * @return string 
+     */
+    protected function getWebDirectory()
+    {
+    
+        return 'uploads/images';
+    }
+    
+    /**
+     * Get image upload directory
+     *
+     * @return string 
+     */
+    protected function getUploadDirectory()
+    {
+    
+        return __DIR__ . '/../../../../web/' . $this->getWebDirectory();
+    }
+    
+    /**
+     * Get image path for the web
+     *
+     * @return string
+     */
+    protected function getPath()
+    {
+    
+        return $this->getWebDirectory() . '/' . $this->getFilename();
+    }
+    
+    /**
+     * Get absolute image path (to delete or move)
+     *
+     * @return string
+     */
+    protected function getAbsolutePath()
+    {
+    
+        return $this->getUploadDirectory() . '/' . $this->getFilename();
+    }
+
+    /**
+     * Uplaod function
+     * This method is called automatically before peristing an object in the database
+     *
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->file) {
+            return;
+        }
+        
+        $this->setFilename(sha1(uniqid(mt_rand(), true)) . '.' . $this->file->guessExtension());
+        $this->file->move($this->getUploadDirectory(), $this->getFilename());
+
+        unset($this->file);
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }
+    
+    /**
+     * Set file
+     *
+     * @param Symfony\Component\HttpFoundation\File\UploadedFile
+     * @return imageFile
+     */
+    public function setFile(UploadedFile $file)
+    {
+        $this->file = $file;
+    
+        return $this;
+    }
+
+    /**
+     * Get file
+     *
+     * @return Symfony\Component\HttpFoundation\File\UploadedFile
+     */
+    public function getFile() {
+    
+        return $this->file;
+    }
+    
+    //
+    // Doctrine-created accessors
+    //
 
     /**
      * Get id
@@ -107,14 +233,14 @@ class ImageFile
     {
         return $this->display;
     }
-    
+
     /**
      * Set project
      *
      * @param \Ben\CoreBundle\Entity\Project $project
      * @return imageFile
      */
-    public function setProject(\Ben\CoreBundle\Entity\Project $project)
+    public function setProject(Project $project)
     {
         $this->project = $project;
     
