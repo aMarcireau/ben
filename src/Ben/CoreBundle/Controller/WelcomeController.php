e2622ad9befbaca7ee9fa2bc55a79937dab46368
Zboubidoo <alexandre.marcireau@student.ecp.fr>
2014-02-24 18:52:08 +0100

    Add welcome page logic

--- src/Ben/CoreBundle/Controller/WelcomeController.php
+++ src/Ben/CoreBundle/Controller/WelcomeController.php
@@ -12,13 +12,53 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class WelcomeController extends Controller
 {
     /**
+     * Welcome page
+     *
      * @Route("/")
      * @Template()
      */
     public function indexAction()
     {
         $imageFileRepository = $this->getDoctrine()->getRepository("BenCoreBundle:ImageFile");
+        
+        $backgroundFiles = array();
+        $backgroundFiles = $imageFileRepository->findByDisplay('background');
+        $backgroundFile = $backgroundFiles[array_rand($backgroundFiles, 1)];
+        
+        $galleryFiles = array();
+        $galleryFiles  = $imageFileRepository->findByDisplay('gallery');
+        foreach($imageFileRepository->findByDisplay('both') as $imageFile) {
+            $backgroundFiles[] = $imageFile;
+            $galleryFiles[]  = $imageFile;
+        }
+        $this->orderByProjectDate($galleryFiles);
+        
+        return array(
+            'backgroundFile' => $backgroundFile,
+            'galleryFiles'  => $galleryFiles,
+        );
+    }
     
-        return array();
+    /**
+     * Order an array of image files by associated project date, then name
+     *
+     */
+    public function orderByProjectDate($imageFileArray) {
+        uasort($imageFileArray, array($this, 'imageFileProjectDateCompare'));
+    }
+    
+    /**
+     * Return true if the first image file's project is more recent than the second
+     */
+    public function imageFileProjectDateCompare($imageFile1, $imageFile2) {
+
+        $date1 = $imageFile1->getProject()->getDate();
+        $date2 = $imageFile2->getProject()->getDate();
+        
+        if ($date1 == $date2) {
+            return strcasecmp($imageFile1->getName(), $imageFile2->getName());
+        }
+        
+        return ($date1 < $date2) ? -1 : 1;
     }
 }
